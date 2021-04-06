a17ff32ef7dab154000c17dd922c60b43c255bd7
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2021-04-06 18:12:08 +0530

    version1

---
+++ index.js
@@ -0,0 +1,8 @@
+let maths=require('./lib');
+
+console.log('hello world');
+
+console.log('maths.plus(2,4)',maths.plus(2,4));
+
+console.log('maths.minus(2,4)',maths.minus(2,4));
+
---
+++ lib.js
@@ -0,0 +1,8 @@
+
+const plus=(a,b)=>a+b;
+
+const minus=(a,b)=>a-b;
+
+module.exports ={
+    plus,minus
+};
